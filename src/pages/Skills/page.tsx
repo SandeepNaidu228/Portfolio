06f7198d407a35320ea33a65c1f55dff03c11857
M V N Sandeep Naidu <106484314+SandeepNaidu228@users.noreply.github.com>
2025-07-28 09:07:55 +0530

    Update page.tsx

--- src/pages/Skills/page.tsx
+++ src/pages/Skills/page.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { useTheme } from "@/provider/page";
-// import { Marquee } from "@/components/magicui/marquee";
+import { Marquee } from "@/components/magicui/marquee";
 import {
   Code2,
   Paintbrush,
